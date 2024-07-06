72f0629a96b17a54047654b205006023fdf64cdc
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-07-06 00:09:16 -0400

    .

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const https = require("https");
-
-https.get('https://teamtreehouse.com/profiles/westonrwigema.json',(res) => {
- let body ="";
+ //print the data
+function printMessage(username, badgeCount, points) {
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+    console.log(message);
+}
+function getProfile(username){
+// connect to api url()
+https.get(`https://teamtreehouse.com/profiles/${username}.json`,
+    (res) => {
+ //read the data
+    let body ="";
     res.on("data",data => {
     body += data.toString();
 });
+ //parse the data
 res.on('end', () => {
-    console.dir(JSON.parse(body));
-})
+    let profile = JSON.parse(body);
+    printMessage(
+        username,
+        profile.badges.length, 
+        profile.points.JavaScript
+    );
 });
+}
+);
+}
+getProfile("westonrwigema");
